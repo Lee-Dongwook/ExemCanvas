5a67609f8f12266baf856a9e6bc3b541798962fe
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-09 17:06:27 +0900

    feat(canvas) 복수 개의 원

--- react-canvas/src/App.tsx
+++ react-canvas/src/App.tsx
@@ -3,69 +3,69 @@ import { drawCircle, type Arc, type Circle } from './canvas/drawCircle';
 import Canvas from './canvas/canvas';
 
 function App() {
-  useEffect(() => {
-    const canvas1 = document.getElementById('canvas1') as HTMLCanvasElement;
-    if (canvas1) {
-      const ctx1 = canvas1.getContext('2d');
-      if (ctx1) {
-        const arc1: Arc = {
-          x: 100,
-          y: 100,
-          radius: 20,
-          startAngle: 0,
-          endAngle: Math.PI * 2,
-        };
+  // useEffect(() => {
+  //   const canvas1 = document.getElementById('canvas1') as HTMLCanvasElement;
+  //   if (canvas1) {
+  //     const ctx1 = canvas1.getContext('2d');
+  //     if (ctx1) {
+  //       const arc1: Arc = {
+  //         x: 100,
+  //         y: 100,
+  //         radius: 20,
+  //         startAngle: 0,
+  //         endAngle: Math.PI * 2,
+  //       };
 
-        const fillStyle1 = 'red';
+  //       const fillStyle1 = 'red';
 
-        const draw1: Circle = {
-          canvas: canvas1,
-          ctx: ctx1,
-          arc: arc1,
-          fillStyle: fillStyle1,
-        };
+  //       const draw1: Circle = {
+  //         canvas: canvas1,
+  //         ctx: ctx1,
+  //         arc: arc1,
+  //         fillStyle: fillStyle1,
+  //       };
 
-        drawCircle(draw1);
-      }
-    }
-    const canvas2 = document.getElementById('canvas2') as HTMLCanvasElement;
-    if (canvas2) {
-      const ctx2 = canvas2.getContext('2d');
-      if (ctx2) {
-        const arc2: Arc = {
-          x: 200,
-          y: 200,
-          radius: 30,
-          startAngle: 0,
-          endAngle: Math.PI * 2,
-        };
+  //       drawCircle(draw1);
+  //     }
+  //   }
+  //   const canvas2 = document.getElementById('canvas2') as HTMLCanvasElement;
+  //   if (canvas2) {
+  //     const ctx2 = canvas2.getContext('2d');
+  //     if (ctx2) {
+  //       const arc2: Arc = {
+  //         x: 200,
+  //         y: 200,
+  //         radius: 30,
+  //         startAngle: 0,
+  //         endAngle: Math.PI * 2,
+  //       };
 
-        const fillStyle2 = 'yellow';
+  //       const fillStyle2 = 'yellow';
 
-        const draw2: Circle = {
-          canvas: canvas2,
-          ctx: ctx2,
-          arc: arc2,
-          fillStyle: fillStyle2,
-        };
+  //       const draw2: Circle = {
+  //         canvas: canvas2,
+  //         ctx: ctx2,
+  //         arc: arc2,
+  //         fillStyle: fillStyle2,
+  //       };
 
-        ctx2.beginPath()
-        ctx2.arc
-        (
-          arc2.x, 
-          arc2.y, 
-          arc2.radius, 
-          arc2.startAngle, 
-          arc2.endAngle, 
-          arc2.clockwise
-        )
+  //       ctx2.beginPath()
+  //       ctx2.arc
+  //       (
+  //         arc2.x, 
+  //         arc2.y, 
+  //         arc2.radius, 
+  //         arc2.startAngle, 
+  //         arc2.endAngle, 
+  //         arc2.clockwise
+  //       )
 
-        ctx2.fillStyle = fillStyle2
-        ctx2.fill()
-        ctx2.closePath()
-      }
-    }
-  }, []);
+  //       ctx2.fillStyle = fillStyle2
+  //       ctx2.fill()
+  //       ctx2.closePath()
+  //     }
+  //   }
+  // }, []);
 
   const draw = (ctx, frameCount) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
@@ -77,9 +77,8 @@ function App() {
 
   return (
     <div>
-      <canvas id='canvas1'></canvas>
       <Canvas draw={draw} />
-      <canvas id='canvas2'></canvas>
+      <Canvas draw={draw} />
     </div>
   );
 }
