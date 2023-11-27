f2b5628110cac2bacc6cf06e3f133a5bf09798f4
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-27 11:26:04 +0900

    refactor(dashboard animation)

--- EXEM/Dashboard/buildDashboardInnerCircle.js
+++
@@ -1,9 +0,0 @@
-const buildDashboardInnerCircle = (config) => {
-    const {center, innerCircle} = config;
-
-    dashboardCanvasContext.beginPath();
-    dashboardCanvasContext.arc(center.x, center.y, innerCircle.radius, innerCircle.angleStart, innerCircle.angleEnd, false);
-    dashboardCanvasContext.lineWidth = innerCircle.lineWidth;
-    dashboardCanvasContext.fillStyle = innerCircle.fillStyle;
-    dashboardCanvasContext.fill();
-}
