b7eb8071c776db116cb72729f3a7e773550c3360
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-29 15:20:44 +0900

    docs(rectWave)

--- EXEM/Component/RectWave/rectWave.js
+++ EXEM/Component/RectWave/rectWave.js
@@ -1,7 +1,15 @@
+/* HTML에서 canvas 요소를 찾고, 2D Context를 얻어와서 해당 캔버스의 너비와 높이에 대한 정보를 저장합니다. */
 const rectWaveCanvas = document.getElementById('rectWave');
 const rectWaveCanvasContext = rectWaveCanvas.getContext('2d');
 const rectWaveCanvasWidth = rectWaveCanvas.width;
 const rectWaveCanvasHeight = rectWaveCanvas.height;
+
+/* rectWave 캔버스에 그려질 사각형과 파형의 그래픽을 구성합니다. 
+   1. center : 사각형과 웨이브가 그려질 중심점의 x,y 좌표를 설정합니다.
+   2. outerRect: 외부 사각형에 대한 너비(width), 높이(height), 선의 두께(lineWidth), 선의 색상(strokeStyle)을 설정합니다.
+   3. innerRect: 내부 사각형에 대한 너비(width), 높이(height), 채우기 색상(fillStyle)을 설정합니다.
+   4. wave : api 응답을 통해 받아들이는 수치 값(data)에 따라 파형의 높낮이를 조절합니다.
+*/
 
 const rectWaveConfig = {
     center: {
@@ -30,6 +38,13 @@ const rectWaveConfig = {
    }
 }
 
+/* 
+  rectWave 캔버스의 내부 사각형을 그리는 함수입니다.
+  rectWaveConfig에 정의된 center와 innerRect 속성을 이용합니다.
+  fillRect 메서드를 사용하여 중심좌표, 너비, 높이를 설정하여 사각형을 그립니다.
+  fillStyle로 채우기 색상을 설정합니다.
+*/
+
 const buildInnerRect = (config) => {
     const { center, innerRect } = config;
 
@@ -41,6 +56,13 @@ const buildInnerRect = (config) => {
         innerRect.height - innerRect.radius
     );
 }
+
+/* 
+  rectWave 캔버스의 외부 사각형을 그리는 함수입니다.
+  rectWaveConfig에 정의된 center와 outerRect 속성을 이용합니다.
+  strokeRect 메서드를 사용하여 중심좌표, 너비, 높이를 설정하여 사각형을 그립니다.
+  strokeStyle로 선의 색상을 설정합니다.
+*/
 
 const buildOuterRect = (config) => {
     const { center, outerRect } = config;
@@ -58,20 +80,25 @@ const buildOuterRect = (config) => {
     rectWaveCanvasContext.stroke();
 }
 
-// Kafka Wave
+/* 
+    파형을 구현하는 부분은 Point, Wave, WaveGroup 3개의 Class로 구현하였습니다.
+    1. Point : 파형의 한 점을 나타내는 클래스입니다.
+    2. Wave : 여러 개의 Point객체를 사용하여 파형을 구현합니다.
+    3. WaveGroup : 여러 개의 Wave 객체를 사용하여 중첩된 파형을 구현합니다.
+*/
 
 class Point {
     constructor(index, x, y) {
         this.x = x;
         this.y = y;
         this.fixedY = y;
-        this.speed = 0.02 * (rectWaveConfig.wave.data / 10);
+        this.speed = 0.02 * (rectWaveConfig.wave.data / 10); // 파형의 속도를 조절하는 부분입니다.
         this.cur = index; 
     }
 
     update() {
         this.cur += this.speed;
-        this.y = this.fixedY + Math.sin(this.cur) * 45;
+        this.y = this.fixedY + Math.sin(this.cur) * 45; //파형의 진폭을 조절하는 부분입니다.
     }
 }
 
@@ -98,7 +125,7 @@ class Wave {
             const point = new Point(
                 this.index + i,
                 this.pointGap * i,
-                this.centerY - rectWaveConfig.wave.data * 4.5,
+                this.centerY - rectWaveConfig.wave.data * 4.5, // 게이지 수치에 따른 파형의 높이 변화 정도를 조절하는 부분입니다.
             );
             this.points[i] = point;
         }
