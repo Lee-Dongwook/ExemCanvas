1a327f3e9df84fa358ca31da4a5b019106870737
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-13 13:53:58 +0900

    feat(circleWaveReactive)

--- EXEM/Reactive/sample/circleWaveReactive.js
+++ EXEM/Reactive/sample/circleWaveReactive.js
@@ -1,22 +1,104 @@
-const waveReactiveCanvas = document.getElementById("circleWaveReactive");
-const waveReactiveCanvasContext = waveReactiveCanvas.getContext("2d");
-const minWidth = 300;
-const minHeight = 300;
+/* HTML에서 canvas 요소를 찾고, 2D Context를 얻어와서 해당 캔버스의 너비와 높이에 대한 정보를 저장합니다. */
+const circleWaveCanvas = document.getElementById("circleWave");
+const circleWaveCanvasContext = circleWaveCanvas.getContext("2d");
+const circleWaveCanvasWidth = circleWaveCanvas.width;
+const circleWaveCanvasHeight = circleWaveCanvas.height;
 
-const waveConfig = {
-  totalWaves: 3,
-  totalPoints: 6,
-  color: ["rgba(0,199,235,0.4)", "rgba(0,146,199,0.4)", "rgba(0,87,158,0.4)"],
-  waves: [],
-  data: 70,
+/* circleWave 캔버스에 그려질 원과 파형의 그래픽을 구성합니다. 
+   1. center : 원과 웨이브가 그려질 중심점의 x,y 좌표를 설정합니다.
+   2. outerCircle: 외부 원에 대한 반지름(radius), 시작각도(angleStart), 끝 각도(angleEnd), 선의 두께(lineWidth), 선의 색상(strokeStyle)을 설정합니다.
+   3. innerCircle: 내부 원에 대한 반지름(radius), 시작각도(angleStart), 끝 각도(angleEnd), 선의 두께(lineWidth), 채우기 색상(fillStyle)을 설정합니다.
+   4. wave : api 응답을 통해 받아들이는 수치 값(data)에 따라 파형의 높낮이를 조절합니다.
+*/
+const circleWaveConfig = {
+  center: {
+    x: circleWaveCanvasWidth,
+    y: circleWaveCanvasHeight,
+  },
+
+  rotateCircle: {
+    radius: (circleWaveCanvasWidth / 600) * 280,
+    angleStart: 0,
+    angleEnd: 2 * Math.PI,
+    lineWidth: 15,
+    strokeStyle: "skyblue",
+  },
+
+  outerCircle: {
+    lineWidth: 50,
+    strokeStyle: "skyblue",
+  },
+
+  innerCircle: {
+    lineWidth: 20,
+    fillStyle: "#000",
+  },
+  wave: {
+    data: 30,
+    color: ["rgba(0,199,235,0.4)", "rgba(0,146,199,0.4)", "rgba(0,87,158,0.4)"],
+    totalWaves: 3,
+    totalPoints: 6,
+  },
 };
 
+/* 
+  circleWave 캔버스의 내부 원을 그리는 함수입니다.
+  circleWaveConfig에 정의된 center와 innerCircle 속성을 이용합니다.
+  arc 메서드를 사용하여 중심좌표, 반지름, 시작/끝 각도, 시계방향 여부를 설정하여 원을 그립니다.
+  lineWidth로 선의 두께를, fillStyle로 채우기 색상을 설정합니다.
+*/
+const buildInnerCircle = (canvasWidth, canvasHeight, config) => {
+  const { innerCircle } = config;
+
+  circleWaveCanvasContext.beginPath();
+  circleWaveCanvasContext.arc(
+    canvasWidth / 2,
+    canvasHeight / 2,
+    canvasWidth * 0.25,
+    0,
+    2 * Math.PI,
+    false
+  );
+  circleWaveCanvasContext.lineWidth = innerCircle.lineWidth;
+  circleWaveCanvasContext.fillStyle = innerCircle.fillStyle;
+  circleWaveCanvasContext.fill();
+};
+
+/* 
+ circleWave 캔버스의 외부 원을 그리는 함수입니다.
+ circleWaveConfig에 정의된 center와 outerCircle 속성을 이용합니다.
+ arc 메서드를 사용하여 중심좌표, 반지름, 시작/끝 각도, 시계방향 여부를 설정하여 원을 그립니다.
+ lineWidth로 선의 두께를, strokeStyle로 선의 색상을 설정합니다.
+*/
+const buildOuterCircle = (canvasWidth, canvasHeight, config) => {
+  const { outerCircle } = config;
+
+  circleWaveCanvasContext.beginPath();
+  circleWaveCanvasContext.arc(
+    canvasWidth / 2,
+    canvasHeight / 2,
+    canvasWidth * 0.25,
+    0,
+    2 * Math.PI,
+    false
+  );
+  circleWaveCanvasContext.lineWidth = outerCircle.lineWidth;
+  circleWaveCanvasContext.strokeStyle = outerCircle.strokeStyle;
+  circleWaveCanvasContext.stroke();
+};
+
+/* 
+    파형을 구현하는 부분은 Point, Wave, WaveGroup 3개의 Class로 구현하였습니다.
+    1. Point : 파형의 한 점을 나타내는 클래스입니다.
+    2. Wave : 여러 개의 Point객체를 사용하여 파형을 구현합니다.
+    3. WaveGroup : 여러 개의 Wave 객체를 사용하여 중첩된 파형을 구현합니다.
+*/
 class Point {
   constructor(index, x, y) {
     this.x = x;
     this.y = y;
     this.fixedY = y;
-    this.speed = 0.02 * (waveConfig.data / 10); // 파형의 속도를 조절하는 부분입니다.
+    this.speed = 0.02 * (circleWaveConfig.wave.data / 10); //파형의 속도를 조절하는 부분입니다.
     this.cur = index;
   }
 
@@ -31,17 +113,17 @@ class Wave {
     this.index = index;
     this.totalPoints = totalPoints;
     this.color = color;
-    this.centerX = waveReactiveCanvas.width / 2;
-    this.centerY = waveReactiveCanvas.height / 2;
     this.points = [];
   }
 
   resize(stageWidth, stageHeight) {
     this.stageWidth = stageWidth;
     this.stageHeight = stageHeight;
+
     this.centerX = stageWidth / 2;
-    this.centerY = stageHeight;
+    this.centerY = stageHeight - circleWaveCanvasHeight / 4; // 파형이 렌더링 되는 중심 y좌표입니다.
     this.pointGap = this.stageWidth / (this.totalPoints - 1);
+
     this.init();
   }
 
@@ -50,8 +132,8 @@ class Wave {
     for (let i = 0; i < this.totalPoints; i++) {
       const point = new Point(
         this.index + i,
-        this.pointGap * i,
-        this.centerY - waveConfig.data * 4.5 // 게이지 수치에 따른 파형의 높이 변화 정도를 조절하는 부분입니다.
+        circleWaveCanvasWidth / 2 + this.pointGap * i * 0.5, // Can6vas 내에서 Point 객체의 첫 시작 위치와 Point 객체 간 간격을 조절하는 부분입니다.
+        this.centerY - circleWaveConfig.wave.data * 3 //게이지 수치에 따른 파형의 높이 변화 정도를 조절하는 부분입니다.
       );
       this.points[i] = point;
     }
@@ -80,9 +162,19 @@ class Wave {
       prevY = this.points[i].y;
     }
 
+    /* 
+      파형의 아랫 부분을 렌더링하는 로직입니다.
+    */
     ctx.lineTo(prevX, prevY);
-    ctx.lineTo(this.stageWidth, this.stageHeight);
-    ctx.lineTo(this.points[0].x, this.stageHeight);
+    // ctx.lineTo(this.points[this.totalPoints - 1].x, this.stageHeight);
+    // ctx.arcTo(
+    //   this.stageWidth / 2,
+    //   this.stageHeight,
+    //   this.points[0].x,
+    //   this.points[0].y,
+    //   0
+    // );
+    // ctx.lineTo(this.points[0].x, this.stageHeight);
     ctx.fill();
     ctx.closePath();
   }
@@ -90,13 +182,11 @@ class Wave {
 
 class WaveGroup {
   constructor() {
-    this.totalWaves = 3;
-    this.totalPoints = 6;
-    this.color = [
-      "rgba(0,199,235,0.4)",
-      "rgba(0,146,199,0.4)",
-      "rgba(0,87,158,0.4)",
-    ];
+    this.totalWaves = circleWaveConfig.wave.totalWaves;
+    this.totalPoints = circleWaveConfig.wave.totalPoints;
+
+    this.color = circleWaveConfig.wave.color;
+
     this.waves = [];
 
     for (let i = 0; i < this.totalWaves; i++) {
@@ -122,10 +212,10 @@ class WaveGroup {
 
 class App {
   constructor() {
-    this.canvas = waveReactiveCanvas;
-    this.ctx = waveReactiveCanvasContext;
-    this.stageWidth = window.innerWidth;
-    this.stageHeight = window.innerHeight / 2;
+    this.canvas = circleWaveCanvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.stageWidth = circleWaveCanvas.width;
+    this.stageHeight = circleWaveCanvas.height / 2;
 
     document.body.appendChild(this.canvas);
 
@@ -138,6 +228,8 @@ class App {
   }
 
   resize() {
+    this.stageWidth = window.innerWidth;
+    this.stageHeight = window.innerHeight;
     this.canvas.width = this.stageWidth;
     this.canvas.height = this.stageHeight;
 
@@ -146,7 +238,9 @@ class App {
 
   animate(time) {
     this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    buildInnerCircle(this.stageWidth, this.stageHeight, circleWaveConfig);
     this.waveGroup.draw(this.ctx);
+    buildOuterCircle(this.stageWidth, this.stageHeight, circleWaveConfig);
     requestAnimationFrame(this.animate.bind(this));
   }
 }
@@ -155,7 +249,47 @@ window.onload = () => {
   new App();
 };
 
-window.addEventListener("resize", () => {
-  const app = new App();
-  app.resize();
-});
+const circleRotateCanvas = document.getElementById("circleRotate");
+const circleRotateCanvasContext = circleRotateCanvas.getContext("2d");
+const circleRotateCanvasWidth = circleRotateCanvas.width;
+const circleRotateCanvasHeight = circleRotateCanvas.height;
+
+const buildRotateOuterCircle = (config, rotateAngle) => {
+  const { center, rotateCircle } = config;
+
+  circleRotateCanvasContext.beginPath();
+  circleRotateCanvasContext.arc(
+    center.x,
+    center.y,
+    rotateCircle.radius,
+    rotateCircle.angleStart + rotateAngle,
+    rotateCircle.angleEnd + rotateAngle,
+    false
+  );
+  circleRotateCanvasContext.lineWidth = rotateCircle.lineWidth;
+  circleRotateCanvasContext.strokeStyle = rotateCircle.strokeStyle;
+  circleRotateCanvasContext.stroke();
+};
+
+let rotate = 0;
+function loop() {
+  circleRotateCanvasContext.clearRect(
+    0,
+    0,
+    circleRotateCanvasWidth,
+    circleRotateCanvasHeight
+  );
+  circleRotateCanvasContext.translate(
+    circleWaveConfig.center.x,
+    circleWaveConfig.center.y
+  );
+  rotate++;
+  circleRotateCanvasContext.setLineDash([1, 135]);
+  circleRotateCanvasContext.setTransform(1, 0, 0, 1, 0, 0);
+
+  buildRotateOuterCircle(circleWaveConfig, (rotate * Math.PI) / 180);
+}
+
+setInterval(() => {
+  loop();
+}, 50);
